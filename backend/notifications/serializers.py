a3aa25f274a9bbae08ea4106468ce79b7499e054
LennyGradient <jeremyli.xie@mail.utoronto.ca>
2023-12-02 15:38:54 -0500

    Add Link To Notifications (FIX)

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -10,12 +10,12 @@ It is fine to not do the above, but instead make a notification read the first t
 class NotificationSerializerC(ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id','notification_type', 'message', 'creation_time', 'last_update_time', 'is_read', 'recipient', 'sender', 'content_type', 'object_id']
+        fields = ['id','notification_type', 'message', 'creation_time', 'last_update_time', 'is_read', 'recipient', 'sender', 'content_type', 'object_id', 'link']
         read_only_fields = ['id','sender', 'content_type', 'object_id']
 
 # Specifically relevant to Read, Update, Destroy (only is_read) modification
 class NotificationSerializerRUD(ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id','notification_type', 'message', 'creation_time', 'last_update_time', 'is_read', 'recipient', 'sender', 'content_type', 'object_id']
+        fields = ['id','notification_type', 'message', 'creation_time', 'last_update_time', 'is_read', 'recipient', 'sender', 'content_type', 'object_id', 'link']
         read_only_fields = ['id','notification_type', 'message', 'creation_time', 'last_update_time', 'recipient', 'sender', 'content_type', 'object_id']
