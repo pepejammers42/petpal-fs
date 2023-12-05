39c1245656aae306cf76aded0b891d2f2aba663d
hknguyen20 <20nguyen.hk@vinuni.edu.vn>
2023-12-05 00:49:20 -0500

    Revert "Revert "added reply to comment""
    This reverts commit 3e5087b66c8a6d76418040ea2af80c5d99460651.

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
-from .models import Comment
+from .models import Comment, Reply
 
+
+class ReplySerializer(ModelSerializer):
+    comment = PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Reply
+        fields = ['comment','id','user','body','created_at','object_id','content_type']
+        read_only_fields = ['comment','user','created_at','object_id','content_type']
 
 class CommentSerializer(ModelSerializer):
+    replies = ReplySerializer(many=True, read_only=True)
     class Meta:
         model = Comment
-        fields = ['id','user','body','created_at','object_id','content_type']
+        fields = ['rating','replies','id','user','body','created_at','object_id','content_type']
         read_only_fields = ['user','created_at','object_id','content_type']
 
