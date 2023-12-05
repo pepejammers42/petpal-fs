eb155c040d9d6bb2e06508fd3e420c078c26d2e6
Nguyen Hoang <91080631+hknguyen20@users.noreply.github.com>
2023-12-05 00:37:09 -0500

    Revert "added reply to backend Comments"

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -1,18 +1,10 @@
 from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
-from .models import Comment, Reply
+from .models import Comment
 
-
-class ReplySerializer(ModelSerializer):
-    comment = PrimaryKeyRelatedField(read_only=True)
-    class Meta:
-        model = Reply
-        fields = ['comment','id','user','body','created_at','object_id','content_type']
-        read_only_fields = ['comment','user','created_at','object_id','content_type']
 
 class CommentSerializer(ModelSerializer):
-    replies = ReplySerializer(many=True, read_only=True)
     class Meta:
         model = Comment
-        fields = ['rating','replies','id','user','body','created_at','object_id','content_type']
+        fields = ['id','user','body','created_at','object_id','content_type']
         read_only_fields = ['user','created_at','object_id','content_type']
 
