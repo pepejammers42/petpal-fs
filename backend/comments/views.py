3e5087b66c8a6d76418040ea2af80c5d99460651
hknguyen20 <20nguyen.hk@vinuni.edu.vn>
2023-12-05 00:38:02 -0500

    Revert "added reply to comment"
    This reverts commit 84da3f4e0643a0723c9ff86c53c981a86ade4084.

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 from rest_framework import generics, permissions, exceptions
 from .models import Comment
 from accounts.models import Shelter, AuthUser
 from applications.models import Application
 from django.contrib.contenttypes.models import ContentType
-from .serializers import CommentSerializer, ReplySerializer
+from .serializers import CommentSerializer
 from rest_framework.pagination import PageNumberPagination
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.views import APIView
-from rest_framework import status
 
 # Create your views here.
 COMMENT_PAGINATION_SIZE = 10 # Number of results to display per page (by default)
@@ -55,42 +52,6 @@ class ShelterCommentListView(generics.ListAPIView):
         return Comment.objects.filter(content_type=content_type, object_id=shelter_id).order_by('-created_at') #descending order
 
 
-class ShelterReplyCreateView(generics.CreateAPIView):
-    serializer_class = ReplySerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        shelter_id = self.kwargs['shelter_id']
-        comment_id = self.kwargs['comment_id']
-        
-        # Ensure the shelter and comment exist
-        get_object_or_404(Shelter, pk=shelter_id)
-        comment = get_object_or_404(Comment, pk=comment_id)
-        content_type = ContentType.objects.get_for_model(Shelter)
-        serializer.save(user=self.request.user, comment=comment, content_type=content_type, object_id=shelter_id)
-  
-
-class ApplicationReplyCreateView(generics.CreateAPIView):
-    serializer_class = ReplySerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
-
-    def perform_create(self, serializer):
-        comment_id = self.kwargs['comment_id']
-        application_id = self.kwargs['application_id']
-        comment = get_object_or_404(Comment, pk=comment_id)
-        application = get_object_or_404(Application, pk=application_id) 
-
-        # Check if the user is related to the application (either seeker or shelter)
-        if application.applicant.email != self.request.user.email and application.pet_listing.shelter.email != self.request.user.email:
-            raise exceptions.PermissionDenied("You do not have permission to reply to this comment.")
-
-        content_type = ContentType.objects.get_for_model(Application)
-        serializer.save(user=self.request.user, comment=comment, content_type=content_type, object_id=application_id)
-        application.save() #to save last update time
-        
 class ApplicationCommentCreateView(generics.CreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
