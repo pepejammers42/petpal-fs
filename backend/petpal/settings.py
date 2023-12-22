e6315c0661db907eac20c3cf9f49dfdced63f091
hknguyen20 <20nguyen.hk@vinuni.edu.vn>
2023-12-22 14:51:41 -0500

    fixed merge

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-rph8!en$(-!&5rrdjb@2zf&4zb4&9sxns$7@++=5evag9^74wi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["18.221.149.61"]
+ALLOWED_HOSTS = ["18.221.149.61", "localhost:8000", "localhost"]
 
 
 # Application definition
@@ -165,4 +165,5 @@ AUTHENTICATION_BACKENDS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://18.221.149.61"
 ]
