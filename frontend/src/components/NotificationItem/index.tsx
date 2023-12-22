7bf8fd05dab9630d44466e6e3f7944eea4c59fab
hknguyen20 <20nguyen.hk@vinuni.edu.vn>
2023-12-22 22:33:51 +0700

    Refactored the notification page into Notification and NotificationItem

--- frontend/src/components/NotificationItem/index.tsx
+++ frontend/src/components/NotificationItem/index.tsx
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+
 import moment from "moment";
-import { ajax_or_login } from '../../ajax';
-import { useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import { TECollapse } from "tw-elements-react";
 
-const NotificationItem = ({ notification }: { notification: any }) => {
-  const [expanded, setExpanded] = useState(false); // Add state for expanded state
-  const navigate = useNavigate();
+interface Notification {
+  id: number;
+  notification_type: string,
+  message: string,
+  creation_time: Date,
+  is_read: boolean,
+  link: string
+}
+
+type NotificationProps = {
+  notification: Notification;
+  idx: number;
+  activeElement: string;
+  setActiveElement: (element: any) => void;
+  setError: (element: any) => void;
+  fetchNotifications: () => void;
+};
 
 
-  const formattedTime = moment(notification.creation_time).fromNow();
+
+
+const NotificationItem: React.FC<NotificationProps> = ({
+  notification,
+  idx,
+  activeElement,
+  setActiveElement,
+  setError,
+  fetchNotifications
+}) => {
+
   const types: { [key: string]: string } = {
     'message': 'message',
     'status_update': 'status update',
@@ -17,44 +41,114 @@ const NotificationItem = ({ notification }: { notification: any }) => {
     'review': 'review',
   };
 
-  const handleExpand = () => {
-    setExpanded(!expanded);
+
+  const handleClick = async (notification: Notification, value: string) => {
+    if (value === activeElement) {
+      // Close
+      setActiveElement("");
+    } else {
+      // Open
+      setActiveElement(value);
+
+      // Set is_read, if the item isn't read
+      if (!notification.is_read) {
+        notification.is_read = true;
+        await updateNotification(notification.id)
+      }
+    }
   };
-  const handleStatusChange = async () => { 
-    await ajax_or_login(`/notifications/${notification.id}/`, {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json',},
-      body: JSON.stringify({
-        is_read: true,
-      })}, navigate);
+
+  const onDelete = async (id: number) => {
+      try {
+          const response = await axios.delete(
+              `/notifications/${id}/`
+          );
+          await fetchNotifications();
+      } catch (err) {
+          setError("Failed to delete notification.");
+          console.error(err);
+      }
+  }
+
+  const updateNotification = async (id: number) => {
+    try {
+      const response = await axios.put(
+        `/notifications/${id}/`,
+        {
+          is_read: true,
+        }
+      );
+      setError(null);
+    } catch (err) {
+      setError("Failed to update notification.");
+      console.error(err);
+    }
   }
 
   return (
-    <li className={`flex items-center px-4 py-2 border-b border-fg-dimmed ${notification.is_read ? 'bg-bg-primary' : 'bg-bg-success'}`}>
-      {/* <span className="text-sm font-bold text-fg-success mr-3">{notification.notification_type}</span> */}
 
-      {/* Notification text and link */}
-      <div className="flex flex-col flex-grow">
-   
-        <span className="text-sm text-fg-primary">You have a new {types[notification.notification_type]}!</span>
-        <span className="text-xs text-primary-300">{formattedTime}</span>
+    <div className="rounded-t-lg border border-neutral-200 bg-white" key={idx}>
+      <h2 className="mb-0" id="headingOne">
+        <button
+          className={`${activeElement === `${idx}` &&
+            `text-primary [box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:!text-primary-400 `
+            } group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none`}
+          type="button"
+          onClick={() => handleClick(notification, `${idx}`)}
+          aria-expanded="true"
+          aria-controls="collapseOne"
+        >
+          <span>
+            <small className={`${notification.is_read ? "hidden" : "text-red-600"}`}>NEW</small> You have a new {types[notification.notification_type]}!
+            <br />
+            <small><i>{moment(notification.creation_time).fromNow()}</i></small>
+          </span>
 
-        {/* Expand button */}
-        <button onClick={handleExpand} className="text-xs text-primary-300 hover:underline">
-          {expanded ? "Collapse" : "Expand"}
-        </button>
 
-        {/* Additional details */}
-        {expanded && (
-          <div>
-           {notification.message}
+          <div className="ml-auto flex">
+            <span
+              className={`${activeElement === `${idx}`
+                ? `rotate-[-180deg]`
+                : `rotate-0 fill-[#212529]  dark:fill-white`
+                } h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </span>
+
           </div>
-        )}
-      </div>
+        </button>
+      </h2>
+      <TECollapse
+        show={activeElement === `${idx}`}
+        className="!mt-0 !rounded-b-none !shadow-none"
+      >
+        <div className="px-5 py-4">
+          {notification.message}
 
-      {/* Unread notification indicator */}
-      {!notification.is_read && <span className="h-2 w-2 bg-secondary-500 rounded-full ml-2"></span>}
-    </li>
+          <br />
+          <br />
+
+          <button
+            className="ml-auto bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => onDelete(notification.id)}>
+            Delete
+          </button>
+        </div>
+      </TECollapse>
+    </div>
   );
 };
 
--- frontend/src/pages/Notifications/index.tsx
+++ frontend/src/pages/Notifications/index.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { useSearchParams } from "react-router-dom";
-import { TECollapse } from "tw-elements-react";
 import axios from "../../api/axios";
 import { DropDown, DropdownProvider } from '../../components/DropDown';
 import SearchPagination from "../../components/SearchPagination";
 import to_url_params from "../../api/urls"
-import { format } from 'date-fns';
-import moment from "moment";
+import NotificationItem from "../../components/NotificationItem";
 
 interface Notification {
     id: number;
@@ -32,14 +30,6 @@ const NotificationsPage: React.FC = () => {
         return temp
     }
 
-    const types: { [key: string]: string } = {
-        'message': 'message',
-        'status_update': 'status update',
-        'pet_listing': 'pet listing',
-        'application': 'application',
-        'review': 'review',
-    };
-
     const [searchParams, setSearchParams] = useSearchParams();
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
@@ -47,22 +37,6 @@ const NotificationsPage: React.FC = () => {
     const [activeElement, setActiveElement] = useState("");
     const [maxCount, setMaxCount] = useState(0);
     const statuses = ["Read", "Unread"]
-
-    const handleClick = async (notification: Notification, value: string) => {
-        if (value === activeElement) {
-            // Close
-            setActiveElement("");
-        } else {
-            // Open
-            setActiveElement(value);
-
-            // Set is_read, if the item isn't read
-            if (!notification.is_read) {
-                notifications[Number(value)].is_read = true;
-                await updateNotification(notification.id)
-            }
-        }
-    };
 
     const query = useMemo(
         () => create_url_params(searchParams)
@@ -101,21 +75,6 @@ const NotificationsPage: React.FC = () => {
         }
     }
 
-    const updateNotification = async (id: number) => {
-        try {
-            const response = await axios.put(
-                `/notifications/${id}/`,
-                {
-                    is_read: true,
-                }
-            );
-            setError(null);
-        } catch (err) {
-            setError("Failed to update notification.");
-            console.error(err);
-        }
-    }
-
     useEffect(() => {
         setIsLoading(true);
         fetchNotifications();
@@ -129,21 +88,7 @@ const NotificationsPage: React.FC = () => {
         return <div>Error: {error}</div>;
     }
 
-    const onDelete = async (id: number) => {
-        try {
-            const response = await axios.delete(
-                `/notifications/${id}/`
-            );
-            await fetchNotifications();
-        } catch (err) {
-            setError("Failed to delete notification.");
-            console.error(err);
-        }
-    }
-
     return (
-
-
         <div className="w-3/4 mx-auto mt-5 flex">
 
             {/* Filter section */}
@@ -162,68 +107,14 @@ const NotificationsPage: React.FC = () => {
                 <h1 className="text-2xl text-fg-primary p-4 text-center">Notifications</h1>
                 <div id="accordionExample">
                     {notifications?.map((notification, idx) => (
-                        <div className="rounded-t-lg border border-neutral-200 bg-white" key={idx}>
-                            <h2 className="mb-0" id="headingOne">
-                                <button
-                                    className={`${activeElement === `${idx}` &&
-                                        `text-primary [box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:!text-primary-400 `
-                                        } group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none`}
-                                    type="button"
-                                    onClick={() => handleClick(notification, `${idx}`)}
-                                    aria-expanded="true"
-                                    aria-controls="collapseOne"
-                                >
-                                    <span>
-                                        <small className={`${notification.is_read ? "hidden" : "text-red-600"}`}>NEW</small> You have a new {types[notification.notification_type]}!
-                                        <br />
-                                        <small><i>{moment(notification.creation_time).fromNow()}</i></small>
-                                    </span>
-
-
-                                    <div className="ml-auto flex">
-                                        <span
-                                            className={`${activeElement === `${idx}`
-                                                ? `rotate-[-180deg]`
-                                                : `rotate-0 fill-[#212529]  dark:fill-white`
-                                                } h-5 w-5 shrink-0 fill-[#336dec] transition-transform duration-200 ease-in-out motion-reduce:transition-none dark:fill-blue-300`}
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth="1.5"
-                                                stroke="currentColor"
-                                                className="h-6 w-6"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                                                />
-                                            </svg>
-                                        </span>
-
-                                    </div>
-                                </button>
-                            </h2>
-                            <TECollapse
-                                show={activeElement === `${idx}`}
-                                className="!mt-0 !rounded-b-none !shadow-none"
-                            >
-                                <div className="px-5 py-4">
-                                    {notification.message}
-
-                                    <br />
-                                    <br />
-
-                                    <button
-                                        className="ml-auto bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                        onClick={() => onDelete(notification.id)}>
-                                        Delete
-                                    </button>
-                                </div>
-                            </TECollapse>
-                        </div>
+                        <NotificationItem
+                            notification={notification}
+                            idx={idx}
+                            activeElement={activeElement}
+                            setActiveElement={setActiveElement}
+                            setError={setError}
+                            fetchNotifications={fetchNotifications}
+                        />
                     ))}
                 </div>
 
